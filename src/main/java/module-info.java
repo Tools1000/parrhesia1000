a20c15b1d0acadf5815401a10db919bfbe8902b8
Alexander Kerner <alexkerner1000@gmail.com>
2023-03-06 20:51:44 +0100

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+open module parrhesia {
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires net.rgielen.fxweaver.core;
+    requires net.rgielen.fxweaver.spring;
+    requires org.apache.commons.lang3;
+    requires org.bitcoinj.core;
+    requires org.slf4j;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+    requires spring.web;
+    requires spring.websocket;
+    requires lombok;
+    requires org.threeten.extra;
+    exports parrhesia1000;
+}
